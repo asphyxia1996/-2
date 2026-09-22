e1b15538beb6c6a279103dc3499651a97139209f
asphyxia1996 <sh3nkkx@gmail.com>
2026-09-22 22:55:30 +0300

    Add files via upload

---
+++ 3.1/3.1. Арифметические операторы.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _3._1.Арифметические_операторы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 17 / 5;
+            //int y = 17 % 5;
+            //Console.WriteLine($"x={x}, y={y}");
+
+
+            //int a = 5;
+            //int res = ++a * 2;
+            //Console.WriteLine($"{res}");
+
+
+            //int a = 5;
+            //int res = a++ * 2;
+            //Console.WriteLine($"{res}");
+
+
+            //int res1 = 7 / 2;
+            //double res2 = 7.0 / 2;
+            //Console.WriteLine($"{res1}, {res2}");
+
+
+            //int res = -15 % 4;
+            //Console.WriteLine($"{res}");
+
+
+            //int x = 10;
+            //x = x++ + ++x;
+            //Console.WriteLine($"{x}");
+
+
+            //int max = int.MaxValue;
+            //int res = checked(max + 1); // System.OverflowException
+            //Console.WriteLine($"{res}");
+
+
+            //int max = int.MaxValue;
+            //int res = unchecked(max + 1);
+            //Console.WriteLine($"{res}");
+
+
+            //double res1 = 1.0 / 0.0;
+            //double res2 = 0.0 / 0.0;
+            //Console.WriteLine($"{res1}, {res2}");
+
+
+            //int a = 8;
+            //int b = 3;
+            //int c = a - b * 2 + a / b;
+            //Console.WriteLine($"{c}");
+        }
+    }
+}
---
+++ 3.2/3.2. Операторы сравнения и равенства.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _3._2.Операторы_сравнения_и_равенства
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool r1 = 5 > 3;
+            //bool r2 = 5 >= 5;
+            //Console.WriteLine($"{r1}, {r2}");
+
+
+            //bool res = "hello" == "hello";
+            //Console.WriteLine($"{res}");
+
+
+            //bool res = double.NaN == double.NaN;
+            //Console.WriteLine($"{res}");
+
+
+            //object a = new int[] { 1 };
+            //object b = new int[] { 1 };
+            //bool r = a == b;
+            //Console.WriteLine($"{r}");
+
+
+            //bool res = 10 != 10.0;
+            //Console.WriteLine($"{res}");
+
+
+            //bool res = null == null;
+            //Console.WriteLine($"{res}");
+
+
+            //bool res = (3 < 5) == (10 >= 20);
+            //Console.WriteLine($"{res}");
+
+
+            //bool res = 4 <= 4 && 5 > 2;
+            //Console.WriteLine($"{res}");
+
+
+            //char c = 'b';
+            //bool res = c > 'a';
+            //Console.WriteLine($"{res}");
+
+
+            //bool r = -0.0 == 0.0;
+            //Console.WriteLine($"{r}");
+        }
+    }
+}
